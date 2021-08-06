0d534d4ef67e5d3c1503e276b6db88a967f24822
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-08-06 18:50:05 +0200

    Run lua after ulx initialized

--- lua/autorun/ulx_yeet.lua
+++ lua/autorun/ulx_yeet.lua
@@ -1,3 +1,5 @@
+AddCSLuaFile( "ulx_yeet/sh_yeet.lua" )
+
 hook.Add( "InitPostEntity", "CFC_ULX_Yeet_InitPostEntity", function()
-    include( "ulx_yeet/ulx_yeet.lua" )
+    include( "ulx_yeet/sh_yeet.lua" )
 end)
