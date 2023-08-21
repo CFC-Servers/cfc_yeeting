818491c1b6d899636939f9307a11bc6c13f4b761
Redox <mail@redox.to>
2023-08-21 19:39:14 +0200

    Use ulx.ragdollPlayer and add ulx freeze support

--- lua/ulx_yeet/sh_yeet.lua
+++ lua/ulx_yeet/sh_yeet.lua
@@ -1,6 +1,4 @@
-if SERVER then
-    ULib.ucl.registerAccess( "physgunragdollplayer", ULib.ACCESS_ADMIN, "Ability to physgun ragdoll other players.", "Other" )
-end
+local IN_ATTACK2 = IN_ATTACK2
 
 local ragdollVelocity = CreateConVar( "ulx_physgun_ragdoll_velocity", 40, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "The velocity required for a physgunned player to turn into a ragdoll on release.", 0 ):GetInt()
 local unragdollVelocity = 0.5 --Increasing this will make players unragdoll too early or create a very fast 
@@ -13,107 +11,9 @@ cvars.AddChangeCallback( "ulx_physgun_ragdoll_maxtime", function( _, _, val )
     ragdollMaxTime = tonumber( val )
 end )
 
-local function savePlayer( ply )
-    local result = {
-        health = ply:Health(),
-        armor = ply:Armor(),
-        weaponData = {}
-    }
-
-    if ply:GetActiveWeapon():IsValid() then
-        result.currentWeapon = ply:GetActiveWeapon():GetClass()
-    end
-
-    local weapons = ply:GetWeapons()
-
-    for _, weapon in ipairs( weapons ) do
-        local className = weapon:GetClass()
-        result.weaponData[className] = {
-            clip1 = weapon:Clip1(),
-            clip2 = weapon:Clip2(),
-            ammo1 = ply:GetAmmoCount( weapon:GetPrimaryAmmoType() ),
-            ammo2 = ply:GetAmmoCount( weapon:GetSecondaryAmmoType() )
-        }
-    end
-
-    ply.cfcYeetData = result
+if CLIENT then
+    CreateClientConVar( "cl_physgunfreezeplayers", 1, true, true, "Whether or not you can freeze players with the physgun." )
 end
-
-local function restorePlayer( ply )
-    local data = ply.cfcYeetData
-    ply:SetParent()
-    ply:SetHealth( data.health )
-    ply:SetArmor( data.armor )
-
-    for weaponClass, weaponInfo in pairs( data.weaponData ) do
-        ply:Give( weaponClass )
-        local weapon = ply:GetWeapon( weaponClass )
-        weapon:SetClip1( weaponInfo.clip1 )
-        weapon:SetClip2( weaponInfo.clip2 )
-        ply:SetAmmo( weaponInfo.ammo1, weapon:GetPrimaryAmmoType() )
-        ply:SetAmmo( weaponInfo.ammo2, weapon:GetSecondaryAmmoType() )
-    end
-
-    if not data.currentWeapon then return end
-    ply:SelectWeapon( data.currentWeapon )
-end
-
-local function unRagdollPlayer( ragdoll )
-    if not IsValid( ragdoll ) then return end
-    local ply = ragdoll.player
-    if not IsValid( ply ) then return end
-    ply:SetParent()
-    ply:UnSpectate()
-    ply:Spawn()
-    restorePlayer( ply )
-
-    ply:SetPos( ragdoll:GetPos() )
-    ply:SetVelocity( ragdoll:GetVelocity() )
-    local yaw = ragdoll:GetAngles().yaw
-    ply:SetAngles( Angle( 0, yaw, 0 ) )
-    ragdoll:Remove()
-    ulx.clearExclusive( ply )
-    ply.yeetRagdoll = nil
-end
-
-local function ragdollPlayer( ply, velocity )
-    if ply.yeetRagdoll then return end
-    savePlayer( ply )
-
-    local ragdoll = ents.Create( "prop_ragdoll" )
-
-    ragdoll:SetModel( ply:GetModel() )
-    ragdoll:SetPos( ply:GetPos() )
-    ragdoll:SetAngles( ply:GetAngles() )
-    ragdoll:SetVelocity( velocity )
-    ragdoll:Spawn()
-
-    local boneCount = ragdoll:GetPhysicsObjectCount() - 1
-
-    for i = 0, boneCount do
-        local bonePhys = ragdoll:GetPhysicsObjectNum( i )
-
-        if IsValid( bonePhys ) then
-            local boneVec, boneAng = ply:GetBonePosition( ragdoll:TranslatePhysBoneToBone( i ) )
-
-            if boneVec and boneAng then
-                bonePhys:SetPos( boneVec )
-                bonePhys:SetAngles( boneAng )
-            end
-            bonePhys:SetVelocity( velocity )
-        end
-    end
-
-    ulx.setExclusive( ply, "ragdolled" )
-    ply.yeetRagdoll = true
-
-    ply:Spectate( OBS_MODE_CHASE )
-    ply:SpectateEntity( ragdoll )
-    ply:StripWeapons()
-
-    return ragdoll
-end
-
 
 local function playerPickup( ply, ent )
     if not ULib.isSandbox() then return end
@@ -127,13 +27,15 @@ local function playerPickup( ply, ent )
 
     local restrictions = {}
 
-    ULib.cmds.PlayerArg.processRestrictions( restrictions, ply, {}, tag and ULib.splitArgs( tag )[ 1 ] )
+    ULib.cmds.PlayerArg.processRestrictions( restrictions, ply, {}, tag and ULib.splitArgs( tag )[1] )
 
     if restrictions.restrictedTargets == false or ( restrictions.restrictedTargets and not table.HasValue( restrictions.restrictedTargets, ent ) ) then
         return
     end
 
     if CLIENT then return true end
+
+    ply.cfCIsHoldingPlayer = ent
 
     ent:SetMoveType( MOVETYPE_NONE )
 
@@ -169,6 +71,8 @@ if CLIENT then return end
 local function playerDrop( ply, ent )
     if not ent:IsPlayer() then return end
 
+    ply.cfCIsHoldingPlayer = nil
+
     hook.Remove( "Tick", "CFC_Yeet_TickHolding_" .. ent:SteamID64() )
 
     local newVelocity = ent.cfcYeetSpeed
@@ -177,44 +81,41 @@ local function playerDrop( ply, ent )
 
     if not ent:Alive() then return end
     if ent.ragdoll then return end
-    local access = ULib.ucl.query( ply, "physgunragdollplayer" )
-    if not access then return end
 
     if newVelocity:Length() < ragdollVelocity then return end
 
     timer.Simple( 0, function()
         if not IsValid( ent ) then return end
 
-        local ragdoll = ragdollPlayer( ent, newVelocity * 50 )
+        ulx.ragdollPlayer( ent )
+
+        local ragdoll = ent.ragdoll
         if not IsValid( ragdoll ) then return end
+
         ragdoll.player = ent
         ragdoll.cooldown = CurTime() + 1
-
-        timer.Simple( ragdollMaxTime, function()
-            unRagdollPlayer( ragdoll )
-        end )
 
         local steamId = ent:SteamID64()
         local hookName = "CFC_Yeet_RagdollTick_" .. steamId
 
+        timer.Simple( ragdollMaxTime, function()
+            if not IsValid( ent ) or not IsValid( ent.ragdoll ) then return end
+            hook.Remove( "Tick", hookName )
+            ulx.unragdollPlayer( ent )
+        end )
+
         hook.Add( "Tick", hookName, function()
-            if not IsValid( ragdoll ) then
+            if not IsValid( ent ) or not IsValid( ragdoll ) then
                 hook.Remove( "Tick", hookName )
                 ent:Spawn()
-                ent.yeetRagdoll = nil
-                return
-            end
-
-            if not IsValid( ragdoll.player ) then
-                ragdoll:Remove()
-                hook.Remove( "Tick", hookName )
                 return
             end
 
             if ragdoll.cooldown > CurTime() then return end
             if ragdoll:GetVelocity():Length() > unragdollVelocity then return end
             hook.Remove( "Tick", hookName )
-            unRagdollPlayer( ragdoll )
+
+            ulx.unragdollPlayer( ent )
         end )
     end )
 end
@@ -223,4 +124,31 @@ hook.Add( "PhysgunDrop", "ulxPlayerDrop", playerDrop )
 
 hook.Add( "CanPlayerSuicide", "ulxYeetCanSuicideCheck", function( ply )
     if ply.yeetRagdoll then return false end
+end )
+
+hook.Add( "KeyPress", "ulxPlayerPickupFreeze", function(ply, key )
+    if key ~= IN_ATTACK2 then return end
+    if ply:GetWeapon( "weapon_physgun" ) ~= ply:GetActiveWeapon() then return end
+    if ply:GetInfoNum( "cl_physgunfreezeplayers", 1 ) ~= 1 then return end
+
+    local heldPlayer = ply.cfCIsHoldingPlayer
+
+    if heldPlayer then
+        local access = ULib.ucl.query( ply, "ulx freeze" )
+        if not access then return end
+
+        ply:ConCommand( "ulx freeze " .. heldPlayer:GetName() )
+    else
+        local trace = ply:GetEyeTrace()
+        local ent = trace.Entity
+
+        if not IsValid( ent ) then return end
+        if not ent.frozen then return end
+        if not ent:IsPlayer() then return end
+
+        local access = ULib.ucl.query( ply, "ulx freeze" )
+        if not access then return end
+
+        ply:ConCommand( "ulx unfreeze " .. ent:GetName() )
+    end
 end )
